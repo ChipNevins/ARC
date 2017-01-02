7d5d6f6fb9bb316c82b116e79b27ed70cc2c11b0
Chip Nevins <chip.nevins@gmail.com>
2017-01-02 14:22:21 -0500

    remove 3-24 deploy

--- XcopyDeploy/3-24/copy_previous_version.bat
+++
@@ -1,7 +0,0 @@
-rem  Copy_previous_version.bat
-
-copy p:\hla\pbrun\3-16\hla.exe c:\hla\pbrun\hla.exe
-copy p:\hla\pbrun\3-16\*.ini c:\hla\pbrun\*.ini
-copy p:\hla\pbrun\3-16\*.pbd c:\hla\pbrun\*.pbd
-copy p:\hla\pbrun\3-16\*.bmp c:\hla\pbrun\*.bmp
-
